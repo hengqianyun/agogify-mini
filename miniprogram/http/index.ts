28062422c96b48a48e9eab5aa35bced5f04be59a
hengqianyun <hengsanshi@163.com>
2021-12-31 15:42:44 +0800

    更新服务器地址

--- miniprogram/http/index.ts
+++ miniprogram/http/index.ts
@@ -1,9 +1,10 @@
 import http from "../libs/http";
 
 // export const BASEURL = wx.getAccountInfoSync().miniProgram.envVersion === 'release' ? 'https://api.agogify.com/api/v2/' : 'https://192.168.20.188:8000/api/v2/'
-export const BASEURL = wx.getAccountInfoSync().miniProgram.envVersion === 'release' ? 'https://api.agogify.com/api/v2/' : 'https://api.agogify.com/api/v2/'
+// export const BASEURL = wx.getAccountInfoSync().miniProgram.envVersion === 'release' ? 'https://api.agogify.com/api/v2/' : 'https://api.agogify.com/api/v2/'
+export const BASEURL = wx.getAccountInfoSync().miniProgram.envVersion === 'release' ? 'https://rss.vieewer.com/api/v2/' : 'https://rss.vieewer.com/api/v2/'
 
-export const IMAGEBASEURL = wx.getAccountInfoSync().miniProgram.envVersion === 'release' ? 'https://api.agogify.com/media/image/' : 'https://api.agogify.com/media/image/' 
+export const IMAGEBASEURL = wx.getAccountInfoSync().miniProgram.envVersion === 'release' ? 'https://rss.vieewer.com/media/image/' : 'https://rss.vieewer.com/media/image/' 
 
 enum SUCCESS_STATUS_CODE_DEF { OK = 200, Created = 201, Accepted = 202, no_content = 204 }
 
