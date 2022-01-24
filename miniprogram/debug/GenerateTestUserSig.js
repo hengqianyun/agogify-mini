4d091b4bba60776dd2b25ca4c272b141f54a4b30
hengqianyun <hengsanshi@163.com>
2022-01-24 13:38:37 +0800

    修改trtc配置

--- miniprogram/debug/GenerateTestUserSig.js
+++ miniprogram/debug/GenerateTestUserSig.js
@@ -6,7 +6,8 @@ import LibGenerateTestUserSig from './lib-generate-test-usersig-es.min.js'
  * 进入腾讯云实时音视频[控制台](https://console.cloud.tencent.com/rav ) 创建应用，即可看到 SDKAppId，
  * 它是腾讯云用于区分客户的唯一标识。
  */
-const SDKAPPID = 1400572600
+// const SDKAPPID = 1400572600
+const SDKAPPID = 1400626008
 
 
 /**
@@ -28,7 +29,8 @@ const EXPIRETIME = 604800
  * 注意：该方案仅适用于调试Demo，正式上线前请将 UserSig 计算代码和密钥迁移到您的后台服务器上，以避免加密密钥泄露导致的流量盗用。
  * 文档：https://cloud.tencent.com/document/product/647/17275#Server
  */
-const SECRETKEY = '82c005dd6f68c4586f705acc3bbabba1ee8ef3a0adf0a9352afa1b1e98c2fcb0'
+// const SECRETKEY = '82c005dd6f68c4586f705acc3bbabba1ee8ef3a0adf0a9352afa1b1e98c2fcb0'
+const SECRETKEY = '96c2cd3ddc461f7e1afe2bc1bec157f81f482c02e620c478323177c531836f15'
 
 /*
  * Module:   GenerateTestUserSig
