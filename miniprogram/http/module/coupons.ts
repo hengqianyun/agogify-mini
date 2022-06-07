3fa0969d7253e2e6e89d60bc2316925765343f83
hengqianyun <hengsanshi@gmail.com>
2022-06-07 13:16:45 +0800

    coupon

--- miniprogram/http/module/coupons.ts
+++ miniprogram/http/module/coupons.ts
@@ -1,7 +1,8 @@
-import { get } from "../index"
+import { get, put } from "../index"
 
  const couponsModule = {
-   getCoupons:  async() =>  await get('store/promotion-coupons', )
+   getCoupons:  async(params: any) =>  await get<any, any>('store/promotion-coupons', params),
+   getGiftCoupon: async(code: string) => await put<any, any>(`store/promotion-coupons/gift/${code}`)
  }
 
  export default couponsModule
--- miniprogram/pages/coupons/coupons.ts
+++ miniprogram/pages/coupons/coupons.ts
@@ -1,3 +1,6 @@
+import couponsModule from "../../http/module/coupons"
+import { timeFormat } from "../../utils/util"
+
 // pages/coupons/coupons.ts
 Page({
 
@@ -5,14 +8,15 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    coupons: [],
+    cdk: ''
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad() {
-
+    this.queryCoupons()
   },
 
   /**
@@ -62,5 +66,61 @@ Page({
    */
   onShareAppMessage() {
 
-  }
+  },
+  async queryCoupons() {
+    const res = await couponsModule.getCoupons({})
+    const list = res.data['hydra:member']
+    this.setData({
+        coupons: list.map((e: any) => {
+            const time = new Date(Date.parse(e.expiresAt))
+            const timeStr = timeFormat(time, 'yyyy-MM-dd') + " " + timeFormat(time, 'hh:mm') + ':00 到期'
+            const promotion = e.promotion
+            let off = ''
+            let isOff = true
+            if (promotion.name.includes('%')) {
+                off = 100 - promotion.name.split('%')[0] + ''
+            } else {
+                isOff = false
+                off = promotion.name.split(' EUR')[0]
+            }
+            return {
+                ...e,
+                timeStr,
+                isOff,
+                off
+            }
+        })
+    })
+  },
+
+  handleInput(event: WechatMiniprogram.TouchEvent) {
+    const { value } = event.detail as { value: string }
+    this.setData({
+      cdk: value
+    })
+  },
+
+  async handleCommit() {
+      if (this.data.cdk === '') {
+          return
+      }
+      try {
+          await couponsModule.getGiftCoupon(this.data.cdk)
+          wx.showToast({
+            title: '兑换成功',
+            icon: 'success',
+            duration: 2000
+        })
+        this.setData({
+            cdk: ''
+        })
+        this.queryCoupons()
+      } catch (err) {
+          wx.showToast({
+              title: '兑换失败',
+              icon: 'error',
+              duration: 2000
+          })
+      }
+  } 
 })
