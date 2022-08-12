0354a8162f5f59619f2f88b18514b3b0d48af766
hengqianyun <hengsanshi@gmail.com>
2022-08-12 12:32:18 +0800

    orderList

--- miniprogram/pages/orderList/orderList.ts
+++ miniprogram/pages/orderList/orderList.ts
@@ -75,10 +75,10 @@ Page({
    */
   onPullDownRefresh() {
     if (this.data.active == 0) {
-        this.queryReadyList(true)
-      } else {
-        this.queryShipedList(true)
-      }
+      this.queryReadyList(true)
+    } else {
+      this.queryShipedList(true)
+    }
   },
 
   /**
@@ -102,15 +102,15 @@ Page({
   handleOrderTap({ currentTarget }: WechatMiniprogram.TouchEvent) {
     const { token } = currentTarget.dataset as { token: string }
     if (this.data.active === 1) {
-        const item = this.data.orderLists[this.data.active].find(e => e.tokenValue === token)
-        wx.navigateTo({
-            url: '../orderDetail/orderDetail?token=' + token + '&trackingCode=' + item?.shipments[0].notes.response.dhlResponse.shipmentTrackingNumber
-          })
+      const item = this.data.orderLists[this.data.active].find(e => e.tokenValue === token)
+      wx.navigateTo({
+        url: '../orderDetail/orderDetail?token=' + token + '&trackingCode=' + item?.shipments[0].notes.response.dhlResponse.shipmentTrackingNumber
+      })
     } else {
 
-        wx.navigateTo({
-          url: '../orderDetail/orderDetail?token=' + token
-        })
+      wx.navigateTo({
+        url: '../orderDetail/orderDetail?token=' + token
+      })
     }
   },
 
@@ -119,7 +119,7 @@ Page({
       title: '加载中...'
     })
     if (isRefresh) {
-        this.data.pageInfo1.page = 1
+      this.data.pageInfo1.page = 1
     }
     try {
       const resData = await orderModule.queryOrderList(this.data.pageInfo1)
@@ -127,7 +127,7 @@ Page({
       const readyList = [] as PageOrders[]
       for (let item of list) {
         if (item.store.logo?.path) {
-          item.store.logo.path = IMAGEBASEURL+ IMAGEPATHS.storeSmall1x + item.store.logo.path
+          item.store.logo.path = IMAGEBASEURL + IMAGEPATHS.storeSmall1x + item.store.logo.path
         }
         item.updatedAt = formatTime(new Date(Date.parse(item.updatedAt)))
         try {
@@ -151,6 +151,7 @@ Page({
         duration: 2000,
       })
     } finally {
+      wx.stopPullDownRefresh()
       wx.hideLoading()
     }
 
@@ -161,7 +162,7 @@ Page({
       title: '加载中...'
     })
     if (isRefresh) {
-        this.data.pageInfo2.page = 1
+      this.data.pageInfo2.page = 1
     }
     try {
       const resData = await orderModule.queryOrderList(this.data.pageInfo2)
@@ -169,7 +170,7 @@ Page({
       const shipedList = [] as PageOrders[]
       for (let item of list) {
         if (item.store.logo?.path) {
-          item.store.logo.path = IMAGEBASEURL+ IMAGEPATHS.storeNormal1x + item.store.logo.path
+          item.store.logo.path = IMAGEBASEURL + IMAGEPATHS.storeNormal1x + item.store.logo.path
         }
         item.updatedAt = formatTime(new Date(Date.parse(item.updatedAt)))
         shipedList.push({ ...item, jsonNotes: JSON.parse(item.notes)[item.items[0].id] })
@@ -188,13 +189,14 @@ Page({
         duration: 2000,
       })
     } finally {
+      wx.stopPullDownRefresh()
       wx.hideLoading()
     }
 
   },
 
   onChange({ detail }: WechatMiniprogram.TouchEvent) {
-    const { index } = detail as unknown as { index: number}
+    const { index } = detail as unknown as { index: number }
     if (index == 1 && this.data.orderLists.length == 1) {
       this.queryShipedList(true)
     }
