3302c7bbeed8128328f329349f0ec551d03d989f
hengqianyun <hengsanshi@gmail.com>
2022-08-15 18:01:18 +0800

    data

--- miniprogram/pages/classification/classification.ts
+++ miniprogram/pages/classification/classification.ts
@@ -26,7 +26,7 @@ Page({
       { text: '店铺类型', value: 'all' },
       { text: '专卖店', value: 'monobrand_store' },
       { text: '百货商店', value: 'department_store' },
-      { text: '买手店', value: 'multi_brand_store' },
+      { text: '集合店', value: 'multi_brand_store' },
       { text: '中古店', value: 'vintage_store' },
     ],
     classification: '全部',
